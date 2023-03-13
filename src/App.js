124f79f3d1b6e6789ae239b24fd53d66db010a72
sam000urai <sam000urai@gmail.com>
2023-03-13 22:30:32 +0900

    add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [displayText, setDisplayText] = useState('');
+
   const increment = () => {
     setCount(count + 1);
     console.log(count);
@@ -17,12 +20,31 @@ const App = () => {
     setCount(0);
   };
 
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Input text: ${text}`);
+    setDisplayText(text);
+    setText('');
+  };
+
   return (
     <div>
       <button onClick={increment}>いいね</button>
       <button onClick={decrement}>よくないね</button>
       <button onClick={reset}>reset</button>
       <p>Count: {count}</p>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleInputChange} />
+        <button type="submit">hello</button>
+      </form>
+
+      <p>Text: {displayText}</p>
+
     </div>
   );
 }
