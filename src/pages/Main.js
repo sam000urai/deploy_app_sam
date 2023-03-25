650421beeeaf2b61362f7c7e4a069f120036a9c6
sam000urai <sam000urai@gmail.com>
2023-03-25 11:04:51 +0900

    add

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,34 @@
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+
 const Main = () => {
-    const navigate = useNavigate()
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                navigate('/');
+            }
+        });
+    }, [navigate]);
+
     const handleClick = () => {
         navigate('/');
     };
+
     return (
         <div>
             <h1>Main</h1>
-            <Button variant="outlined" onClick={handleClick}>戻る</Button>
+            <p>{user?.email}でログイン中</p>
+            <Button variant="outlined" onClick={handleClick}>ログアウト</Button>
         </div>
     );
 };
+
 export default Main;
